93e5c97810ecd656ce4480fbbbfc2719791a4221
Nayanpawar03 <155285726+Nayanpawar03@users.noreply.github.com>
2024-10-24 14:56:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function transferFunds() {
+    alert('Transfer Funds clicked');
+    // Add functionality for transferring funds here
+}
+
+function addMoney() {
+    alert('Add Money clicked');
+    // Add functionality for adding money to account here
+}
+
+function viewHistory() {
+    alert('View Transaction History clicked');
+    // Add functionality for viewing transaction history here
+}
+
+function logout() {
+    alert('Logging out...');
+    // Add functionality for logging out here
+}
